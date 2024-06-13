ac21ff6be8ebbb8eaeca95aac277f149a5cc0687
banri <banri.yanahama@gmail.com>
2024-06-14 00:42:35 +0900

    add logger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import Web3 from "web3";
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'log/app.log' })
+  ]
+});
 
 async function getDeployedContracts(deployerAddress: string): Promise<string[]> {
   const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFRA_API_KEY}`);
@@ -22,7 +37,7 @@ async function getDeployedContracts(deployerAddress: string): Promise<string[]> 
 
       // Print progress
       if (blockNumber % BigInt(100) === BigInt(0)) {
-        console.log(`Scanning block ${blockNumber}...`);
+        logger.info(`Scanning block ${blockNumber}...`);
       }
 
       // Stop scanning if we have reached the block that UniSwapX was called for the first time
@@ -38,19 +53,19 @@ async function getDeployedContracts(deployerAddress: string): Promise<string[]> 
 function main() {
   const args = process.argv.slice(2);
   if (args.length !== 1) {
-    console.log("You must pass exactly one wallet address as an argument.");
+    logger.info("You must pass exactly one wallet address as an argument.");
     process.exit(1);
   }
 
   const walletAddress = args[0];
-  console.log(`Fetching all contract addresses for wallet: ${walletAddress}`);
+  logger.info(`Fetching all contract addresses for wallet: ${walletAddress}`);
 
   getDeployedContracts(walletAddress)
     .then((contracts) => {
-      console.log('Deployed contracts:', contracts);
+      logger.info('Deployed contracts:', contracts);
     })
     .catch((error) => {
-      console.error('Error:', error);
+      logger.error('Error:', error);
     });
 }
 
